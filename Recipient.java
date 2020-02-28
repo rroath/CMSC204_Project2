03c8c580e3c401cecaeeaba90b8aa827b71a3746
rroath <54912392+rroath@users.noreply.github.com>
2020-02-28 13:58:04 -0500

    Add files via upload

---
+++ Recipient.java
@@ -0,0 +1,13 @@
+
+public class Recipient {
+private String name;
+	
+	public Recipient(String s) {
+		this.name=s;
+	}
+	
+	public Recipient() {
+		name="NONE";
+	}
+
+}
---
+++ Volunteer.java
@@ -0,0 +1,13 @@
+
+public class Volunteer {
+	private String name;
+	
+	public Volunteer(String s) {
+		this.name=s;
+	}
+	
+	public Volunteer() {
+		name="NONE";
+	}
+
+}
